db3a0e9c103d7f9b19fecad42bf1f982d94d6c68
yxx2 <y15138430798@163.com>
2023-07-28 16:31:05 +0800

    :bug: ip归属地解析 找不到文件bug修复

--- common/common-core/src/main/java/com/yxx/common/utils/ip/AddressUtil.java
+++ common/common-core/src/main/java/com/yxx/common/utils/ip/AddressUtil.java
@@ -1,6 +1,7 @@
 package com.yxx.common.utils.ip;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.lionsoul.ip2region.xdb.Searcher;
 
 import java.util.Objects;
@@ -12,6 +13,7 @@ import java.util.Objects;
  * @classname AddressUtil
  * @since 2023/07/24
  */
+@Slf4j
 public class AddressUtil {
 
     /**
@@ -35,4 +37,25 @@ public class AddressUtil {
         //默认返回空字符串
         return "";
     }
+
+    /**
+     * 根据IP地址查询登录来源
+     * 注：⚠️ 上线服务器时用这个方法，可以避免找不到ip2region.xdb文件,  上线时，在jar包所在的目录，放一份ip2region.xdb文件
+     *
+     * @param ip ip
+     * @return {@link String }
+     * @author yxx
+     */
+    /*public static String getCityInfo(String ip) {
+        try {
+            String path = System.getProperty("user.dir");
+            FileInputStream fileInputStream = new FileInputStream(path + "/ip2region.xdb");
+            Searcher searcher = Searcher.newWithBuffer(IoUtil.readBytes(fileInputStream));
+            String search = searcher.search(ip);
+            String[] split = search.split("\\|");
+            return Arrays.toString(split);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }*/
 }
