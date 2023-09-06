3c1eeb360568b71d6f3ec5b29717c6ffc822c7bd
yxx2 <y15138430798@163.com>
2023-09-06 11:26:29 +0800

    :sparkles: 增加socket框架 netty

---
+++ common/common-framework/src/main/java/com/yxx/framework/socket/WebsocketMessageHandler.java
@@ -0,0 +1,57 @@
+package com.yxx.framework.socket;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketCloseStatus;
+import io.netty.handler.codec.http.websocketx.WebSocketFrame;
+
+/**
+ * @author yxx
+ * @classname WebsocketMessageHandler
+ * @since 2023/09/06
+ */
+@Slf4j
+@Sharable
+@Component
+@RequiredArgsConstructor
+public class WebsocketMessageHandler extends SimpleChannelInboundHandler<WebSocketFrame> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebsocketMessageHandler.class);
+
+//    private final DiscardService discardService;
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, WebSocketFrame msg) {
+        if (msg instanceof TextWebSocketFrame textWebSocketFrame) {
+            // 业务层处理数据
+//            this.discardService.discard(textWebSocketFrame.text());
+            log.info("textWebSocketFrame.text():{}", textWebSocketFrame.text());
+            // 响应客户端
+            ctx.channel().writeAndFlush(new TextWebSocketFrame("我收到了你的消息：" + System.currentTimeMillis()));
+        } else {
+            // 不接受文本以外的数据帧类型
+            ctx.channel().writeAndFlush(WebSocketCloseStatus.INVALID_MESSAGE_TYPE).addListener(ChannelFutureListener.CLOSE);
+        }
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        super.channelInactive(ctx);
+        LOGGER.info("链接断开：{}", ctx.channel().remoteAddress());
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        super.channelActive(ctx);
+        LOGGER.info("链接创建：{}", ctx.channel().remoteAddress());
+    }
+}
